33d3e01ddcaea5657df0dadc13633b2a1dd4e739
nnavnita <navnitanandakumar@gmail.com>
2022-01-09 07:00:42 +1100

    add more info to card

--- src/components/FindHookah/HookahLocationCard.js
+++ src/components/FindHookah/HookahLocationCard.js
@@ -1,9 +1,7 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-// import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
+import { Card, CardContent, CardActions, CardMedia, Typography, Button, Grid } from "@mui/material";
+import { Star, Phone, Public } from "@mui/icons-material";
+import { yellow } from '@mui/material/colors';
 
 const HookahLocationCard = ({ item, setActiveListItem, activeListItem }) => {
   return (
@@ -15,16 +13,40 @@ const HookahLocationCard = ({ item, setActiveListItem, activeListItem }) => {
         component="img"
         height="140"
         image={item.imgUrl}
-        alt="hooka-card"
+        alt="hookah-card"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {item.name}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Rating: {item.rating} / 5
-        </Typography>
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+          <Typography variant="body2" color="text.secondary" sx={{textAlign: "left", width: "60%"}}>
+            {item.address.text}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" sx={{textAlign: "right"}}>
+          <Grid container direction="row" justifyContent="center">
+            <Grid item>
+            {item.rating}
+            </Grid>
+            <Grid item>
+              <Star sx={{ color: yellow[700], fontSize: 20 }} /> 
+            </Grid>
+          </Grid>
+          </Typography>
+        </div>
       </CardContent>
+      {
+        activeListItem === item
+        ? <CardActions style={{ display: "flex", justifyContent: "space-around" }}>
+        <Button
+          style={{ width: "35%", display: "flex", justifyContent: "space-around"}}
+          href={item.website} target="_blank"><Public color="primary" />Visit Website</Button>
+        <Button
+          style={{ width: "35%", display: "flex", justifyContent: "space-around"}}
+          href={`tel:${item.phone}`}><Phone color="primary" /> {item.phone}</Button>
+        </CardActions>
+        : null
+      }
     </Card>
   );
 };
