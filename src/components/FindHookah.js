5a73bcc65b260e8889e13def20030bb323857d3b
nnavnita <navnitanandakumar@gmail.com>
2022-01-09 02:37:11 +1100

    added searchbar

--- src/components/FindHookah.js
+++ src/components/FindHookah.js
@@ -1,13 +1,16 @@
 import "./styles/FindHookah.scss";
 import Map from "./Map";
 import GetHookahList from "./HookahList";
+import SearchBar from "./SearchBar";
 import { dummyLists } from "./DummyHookahList";
 
 const FindHookah = () => {
   const title = "Find hookah near me";
   return (
     <div className="FindHookah">
-      <div className="FindHookah-header">{title}</div>
+      <div className="FindHookah-header">
+        <SearchBar />
+      </div>
       <div className="FindHookah-body">
         <div className="FindHookah-list">
           <GetHookahList list={dummyLists} />
