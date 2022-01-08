6638792fa37854b2c87ec6e7b4f0d0be9a433747
syedm1 <31075630+syedm1@users.noreply.github.com>
2022-01-09 01:09:05 +1100

    Hookah list

---
+++ src/components/DummyHookahList.js
@@ -0,0 +1,64 @@
+export const dummyLists = [
+  {
+    id: 1,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl:
+      "https://media-cdn.tripadvisor.com/media/photo-s/1a/34/28/62/dancers.jpg",
+  },
+  {
+    id: 2,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl:
+      "https://us.123rf.com/450wm/tommisch/tommisch1906/tommisch190601468/125146762-sexy-brunette-model-is-smoking-a-hookah-exhaling-a-smoke-at-a-luxury-night-club-.jpg?ver=6",
+  },
+  {
+    id: 3,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl: "https://i.ytimg.com/vi/I0cnYcFSDAU/maxresdefault.jpg",
+  },
+  {
+    id: 4,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl:
+      "https://st4.depositphotos.com/9904426/21576/i/600/depositphotos_215761116-stock-photo-shisha-party-club-concept-mixer.jpg",
+  },
+  {
+    id: 5,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl:
+      "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F46570156%2F213087556184%2F1%2Foriginal.jpg?auto=compress&s=5a9bf981fc4a1108dda953d94b73eb2a",
+  },
+  {
+    id: 6,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl:
+      "https://media.istockphoto.com/photos/hookah-and-two-glasses-standing-on-bar-counter-at-night-club-party-picture-id1072208092?k=20&m=1072208092&s=612x612&w=0&h=nJsHSzg8RtDbUtM0HGAfxa5i7wJMBL29M-x_F-fM2eg=",
+  },
+  {
+    id: 7,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtRLMi_5hD1j7bqWHalj2t3knX28hH5BJPHiqsNZktaKxkfOaSocJTQ5uT7b9Xhn2pHRw&usqp=CAU",
+  },
+  {
+    id: 8,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl:
+      "https://st4.depositphotos.com/9904426/21576/i/600/depositphotos_215761116-stock-photo-shisha-party-club-concept-mixer.jpg",
+  },
+  {
+    id: 9,
+    name: "Melbourne hooka parlour",
+    rating: "4",
+    imgUrl:
+      "https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:17682?quality=0.8&format=jpg&width=1440&height=810&.jpg",
+  },
+];
