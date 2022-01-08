e0dee4982ef47be03c759523ca2c28367bb5c22c
nnavnita <navnitanandakumar@gmail.com>
2022-01-08 21:46:50 +1100

    basic homepage + ignore yarn lock

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+
+const logo = require('./assets/logo.png');
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Welcome to Hooka.wiki!
         </p>
         <a
           className="App-link"
@@ -15,8 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Visit our Hookah Lab
+        </a>  
       </header>
     </div>
   );
