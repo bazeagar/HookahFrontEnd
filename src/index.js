3db3aba461109d1edf791343ed87620954b3c4b6
nnavnita <navnitanandakumar@gmail.com>
2022-01-08 23:47:54 +1100

    add App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import App from './components/FindHookah';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
